c5bd0e0ad3898a5f7e919d46c4215bcdd767ceae
Selectel Git User <git-user@selectel.ru>
2024-03-21 10:48:28 +0500

    Задача 1

---
+++ task1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Console.WriteLine("Hello, World!");
+// Задача 1: Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N. 
+// Использовать рекурсию, не использовать циклы
+
+Console.Clear();
+void LineOfDigits(int M, int N)
+{
+    if (N < M)
+    {
+        return;
+    }
+    Console.Write($"{M} ");
+    LineOfDigits(M + 1, N);
+    
+}
+Console.WriteLine("Введите первое число промежутка: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число промежутка: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+LineOfDigits(M, N);
